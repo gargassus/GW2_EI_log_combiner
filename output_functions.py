c2f427d4a65e2d7d37a1c57413e9f5d553c09fa1
Drevarr <drevarr@gmail.com>
2025-02-19 13:55:18 -0500

    limit impact of downed healing on Max Healing in `Healer Outgoing` tables

--- output_functions.py
+++ output_functions.py
@@ -2064,10 +2064,10 @@ def build_healer_outgoing_tids(top_stats: dict, skill_data: dict, buff_data: dic
 				skill_name = skill_data.get(skill, {}).get("name", buff_data.get(skill.replace("s", "b"), {}).get("name", ""))
 				skill_icon = skill_data.get(skill, {}).get("icon", buff_data.get(skill.replace("s", "b"), {}).get("icon", ""))
 				entry = f"[img width=24 [{skill_name}|{skill_icon}]]-{skill_name}"
-				max_heal = top_stats['player'][healer]['extHealingStats']['skills'][skill]['max']
 				hits = top_stats['player'][healer]['extHealingStats']['skills'][skill]['hits']
 				total_healing = top_stats['player'][healer]['extHealingStats']['skills'][skill]['healing']
 				avg_healing = total_healing/hits if hits > 0 else 0
+				max_heal = top_stats['player'][healer]['extHealingStats']['skills'][skill]['max'] if total_healing > 0 else 0
 
 				row = f"|{entry} | {hits:,.0f} | {total_healing:,.0f}| {avg_healing:,.0f}| {max_heal:,.0f}| {total_healing/outgoing_healing*100:,.2f}%|"
 
